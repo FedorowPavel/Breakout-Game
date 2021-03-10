2c559d9647485c35a9c78576327645d6b5b49301
FedorowPavel <pasha_fedorow@mail.ru>
2021-03-10 14:19:04 +0200

    rules section styles added

--- js/templates/pages/initial-page.js
+++ js/templates/pages/initial-page.js
@@ -4,9 +4,36 @@ const startTemplate = `
     <button id="rules-btn">Rules</button>
     <div id="rules" class="rules">
         <h3>How to play</h3>
+
         <p>Use your right and left keys to move the paddle to bounce the ball up and break the blocks</p>
         <p>If you miss the ball you loose.</p>
-        <button id="close-rules-btn">Close</button>
+
+        <h3>Handlers</h3>
+
+        <div class="handler">
+            <span class="handler-key enter">enter</span>
+            <span>star game</span>
+        </div>
+
+        <div class="handler">
+            <span class="handler-key"><i class="fas fa-arrow-left"></i></span>
+            <span class="handler-key"><i class="fas fa-arrow-right"></i></span>
+            <span>move paddle</span>
+        </div>
+
+        <div class="handler">
+            <span class="handler-key space">space</span>
+            <span>pause/resume</span>
+        </div>
+
+        <div class="handler">
+            <span class="handler-key space">esc</span>
+            <span>restart</span>
+        </div>
+
+        <button id="close-rules-btn"><i class="fas fa-long-arrow-alt-left"></i> </button>
+
+
     </div>
     <button id="color-mode-btn"><i class="fas fa-lightbulb"></i></button>
 
